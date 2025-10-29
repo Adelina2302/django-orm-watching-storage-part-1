f58655aadf74540735c8a8f728cd8b9c1d4fdd6a
Adelina2302 <adelina2302.dunina@gmail.com>
2025-10-29 01:01:38 -0400

    Filtered and counted active passcards

--- main.py
+++ main.py
@@ -9,4 +9,18 @@ from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
     # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    print(Passcard.objects.all())
+    all_passcards = Passcard.objects.all()
+    first_passcard = all_passcards[0]
+
+    print('owner_name:', first_passcard.owner_name)
+    print('passcode:', first_passcard.passcode)
+    print('created_at:', first_passcard.created_at)
+    print('is_active:', first_passcard.is_active)
+
+
+    active_passcards = Passcard.objects.filter(is_active=True)
+
+    print('Всего пропусков:', len(all_passcards))
+    print('Активных пропусков:', len(active_passcards))
+
