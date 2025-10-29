21d051d32a1a680a030c58413d8c8bdcfe1970db
Adelina2302 <adelina2302.dunina@gmail.com>
2025-10-29 00:59:22 -0400

    Hide database credentials in .env file

--- settings.py
+++ settings.py
@@ -1,18 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'ENGINE': os.getenv('ENGINE'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 TIME_ZONE = 'Europe/Moscow'
-
 USE_TZ = True
